f33a146a1230f9b61b98f4f76edea2dc28ea2887
Shirahamah <hiropi1224s@gmail.com>
2023-07-12 00:00:52 +0900

    💡 コメント追加

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// ()内でルートを指定することができる
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // ()内でルートを指定することができる
   @Get()
   getHello(): string {
     return this.appService.getHello();
