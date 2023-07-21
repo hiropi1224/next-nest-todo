c97a118a67bb32026ebdfbd5e5352125a2288b30
Shirahamah <hiropi1224s@gmail.com>
2023-07-21 23:24:48 +0900

    👔 prismaModuleを追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * DTOとクラスバリデーションを有効化するために必要な処理
+   */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  /**
+   * CORSの設定
+   * ホワイトリストにバックエンドへのアクセスを許可したいフロントエンドのドメインを指定する
+   * フロント、バックエンドでCookieベースでJWTトークンを通信するためにcredentialsをtrueに設定。
+   */
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
-@Injectable()
-export class PrismaService {}
+@Injectable() // PrismaClientのクラスの機能を取り込むためにPrismaClientをextendsで継承する
+export class PrismaService extends PrismaClient {
+  // ConfigServiceをDIする
+  constructor(private readonly config: ConfigService) {
+    // superを使うことで継承しているPrismaClientの中のコンストラクターを参照できる
+    super({
+      // datasourcesのoptionsのdbに今回のpostgresqlのURLパスを設定する
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+}
